26d011c2eaf9a9ac1a273283e0b3d637a844c505
3pnguyen <ethannguyen654wsd@gmail.com>
2025-11-05 11:52:45 -0800

    Add files via upload
    code update 1

---
+++ vex-vscode-projects/seventy/src/robot-config.cpp
@@ -0,0 +1,21 @@
+#include "vex.h"
+#include "quick-config.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+brain Brain;
+motor LeftMotor = motor(LEFT_DRIVE_MOTOR_PORT, false);
+motor RightMotor = motor(RIGHT_DRIVE_MOTOR_PORT, true);
+drivetrain Drivetrain = drivetrain(LeftMotor, RightMotor, 319.18, 355.6, 177.8, mm, 1);
+motor ClawMotor = motor(CLAW_MOTOR_PORT, true);
+motor ArmMotor = motor(ARM_MOTOR_PORT, ratio18_1, false);
+controller Gamepad = controller();
+TFlipFlop ClawMotorToggle = TFlipFlop();
+SlewController DriveAcceleration = SlewController(5, 1.5, 3); //accel and decel reversed lol
+
+void vexcodeInit(void) {
+    ArmMotor.setVelocity(100, rpm);
+    ClawMotor.setVelocity(100, rpm);
+}
